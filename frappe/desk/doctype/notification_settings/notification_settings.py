15e37cf63a95f73e107a2946234b798644600414
Parth Kharwar <kharwarparth@gmail.com>
2021-03-05 13:26:46 +0530

    fix: uses db.set_value to avoid permission issues

--- frappe/desk/doctype/notification_settings/notification_settings.py
+++ frappe/desk/doctype/notification_settings/notification_settings.py
@@ -44,7 +44,7 @@ def create_notification_settings(user):
 
 def toggle_notifications(user, enable=False):
 	if frappe.db.exists("Notification Settings", user):
-		frappe.set_value("Notification Settings", user, 'enabled', enable)
+		frappe.db.set_value("Notification Settings", user, 'enabled', enable)
 
 
 @frappe.whitelist()
